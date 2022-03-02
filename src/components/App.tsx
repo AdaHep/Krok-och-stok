1b8fe0de1591ac506ca4b53341eaf1c52a1d5e01
AdaHep <adam.hepsever@gmail.com>
2022-03-02 10:12:27 +0100

    Fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AgeCheck from "./ageCheck";
-import ProductPageBeer from "./productPageBeer";
-import ProductPageSprit from "./productPageSprit";
-import ProductPageWine from "./productPageWine";
-import Under18 from "./under18";
 import Cart from "./Cart";
+import Shop from "./Shop";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AgeCheck />}></Route>
-        <Route path="productPageBeer" element={<ProductPageBeer />}></Route>
-        <Route path="productPageSprit" element={<ProductPageSprit />}></Route>
-        <Route path="productPageWine" element={<ProductPageWine />}></Route>
-        <Route path="under18" element={<Under18 />}></Route>
-        <Route path="Cart" element={<Cart />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Shop />
+      {/* <Cart /> */}
+    </div>
   );
 }
 export default App;
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AgeCheck from "./ageCheck";
+import Cart from "./Cart";
 import ProductPageBeer from "./productPageBeer";
+import ProductPageSpirits from "./productPageSpirits";
+import ProductPageWine from "./productPageWine";
+import Under18 from "./under18";
 
 function Shop() {
   return (
-    <div>
-      {/* <AgeWeightForm /> */}
-      <ProductPageBeer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AgeCheck />}></Route>
+        <Route path="productPageBeer" element={<ProductPageBeer />}></Route>
+        <Route
+          path="productPageSpirits"
+          element={<ProductPageSpirits />}
+        ></Route>
+        <Route path="productPageWine" element={<ProductPageWine />}></Route>
+        <Route path="Cart" element={<Cart />}></Route>
+        <Route path="under18" element={<Under18 />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
